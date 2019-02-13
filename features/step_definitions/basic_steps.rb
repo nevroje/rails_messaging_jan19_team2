ff45d2ef805aafa6a41d5c22eb29aa104aed2fef
Alecia <aleciabenjamin@gmail.com>
2019-02-13 09:46:21 +0100

    create and pass user sign-up feature test

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,11 @@
+When("I visit the site") do
+    visit root_path
+end
+  
+  Then("I should see {string} page") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I click on {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
