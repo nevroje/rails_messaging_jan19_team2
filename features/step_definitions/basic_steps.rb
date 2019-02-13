20ca740727851fb9b323e434e6a983434af57a9c
Alecia <aleciabenjamin@gmail.com>
2019-02-13 11:22:39 +0100

    passed test to click on sign up

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,10 +2,10 @@ When("I visit the site") do
     visit root_path
 end
   
-  Then("I should see {string}") do |string|
+Then("I should see {string}") do |string|
     expect(page).to have_content string
-  end
+end
   
-  Then("I click on {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+When("I click on {string}") do |string|
+  expect(page).to have_content string
+end
--- features/user_sign_up.feature
+++ features/user_sign_up.feature
@@ -6,4 +6,9 @@ Feature: User sign-up
 Scenario: User visits page to sign-up
     When I visit the site
     Then I should see "CA Mailboxer" 
-    And I click on "sign-up"
+    When I click on "Sign up"
+    And I fill in "name" with "Bob"
+    And I fill in "email" with "bob@bobby.com"
+    And I fill in "password" with "0443431157"
+    And I fill in "password confirmation" with "0443431157"
+
